0ed053a9de58fa1fddff0a29df79d72f6bdbe67a
user1 <user1@DESKTOP-TNJQ777>
2024-01-18 16:09:01 +0900

    pull한 후 수정

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("김동욱바보");
+        
+        int n = 0;
 
         System.out.println("Hello world!");
         System.out.println(" 김도영 ");
+        
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        
+        String str = "";
     }
 }
