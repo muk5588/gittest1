44c547e01d79d7c142c8ece483a7b8b64ca882fd
user1 <user1@DESKTOP-TNJQ777>
2024-01-18 16:22:17 +0900

    class 추가

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+}
+
+
 public class Main {
 	public static void main(String[] args) {
 
@@ -16,6 +32,10 @@ public class Main {
         int num;
 
         num = 1;
+        
+        Point p1 = new Point(1, 2);
+        
+        System.out.println(p1.toString());
+        
     }
-
-	}
+}
