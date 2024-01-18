8f7d13b84eabb8aed079491ac29fb77235eb4574
user1 <user1@DESKTOP-NE1HP2P>
2024-01-18 15:42:59 +0900

    hjhhj

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println(" 김도영 ");
-        System.out.println(" hbhjubhju");
+        System.out.println("gfdg");
     }
 }
