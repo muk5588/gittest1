f21aef67bc93c1a34980773834e88b191e5533e6
user1 <user1@DESKTOP-TRNMQ5J>
2024-01-18 16:43:25 +0900

    출돌 test

--- src/Dongwook.java
+++ src/Dongwook.java
@@ -1,7 +1,16 @@
 
 public class Dongwook {
 	public static void main(String[] args) {
+		
 		System.out.println("안녕하세요");
+		
+		System.out.println(123124);
+		
+		int a = 1;
+		int b = 2;
+		
+		System.out.println(a+b);
+		
 		
 	}
 }
