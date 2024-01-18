4a73802d7fcf385b3e227df6805083337e1fa51c
user1 <user1@DESKTOP-DPTKKHL>
2024-01-18 15:06:24 +0900

    240118_sub(ji ho)

---
+++ src/Sub_Jiho.java
@@ -0,0 +1,13 @@
+
+public class Sub_Jiho {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Radical");
+		System.out.println("Audio");
+		System.out.println("Visual");
+		System.out.println("Experience");
+		
+	}
+
+}
